adba4f8a3c889efd22c18d311bae7f7897ca8816
Tiago Barreto <tiagovalentim@gmail.com>
2020-06-25 23:10:33 -0300

    Adding expiration-completed-listener in order service

---
+++ orders/src/events/listeners/expiration-completed-listener.ts
@@ -0,0 +1,39 @@
+import { Message } from "node-nats-streaming";
+import {
+  Subjects,
+  Listener,
+  ExpirationCompletedEvent,
+  OrderStatus,
+} from "@braves-corp/common";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompletedListener extends Listener<
+  ExpirationCompletedEvent
+> {
+  subject: Subjects.ExpirationCompleted = Subjects.ExpirationCompleted;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompletedEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found.");
+    }
+
+    order.set({ status: OrderStatus.CANCELLED });
+    await order.save();
+
+    const publisher = new OrderCancelledPublisher(this.client);
+    await publisher.publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
