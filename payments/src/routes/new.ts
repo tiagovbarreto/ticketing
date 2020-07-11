05dd9542eab2c1dcf1c92642313f0da522da951f
Tiago Barreto <tiagovalentim@gmail.com>
2020-07-11 16:10:18 -0300

    Adding payment created publisher to payment service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import { body } from "express-validator";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = Router();
 
@@ -48,7 +50,14 @@ router.post(
     });
     await payment.save();
 
-    res.status(201).send({ success: true });
+    const publisher = new PaymentCreatedPublisher(natsWrapper.client);
+    await publisher.publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send(payment);
   }
 );
 
