f84027ad3322fea62ee12b8b1d2caf25c837f449
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-09-02 01:06:20 -0300

    Setting up domain names

--- client/helpers/axios-helper.js
+++ client/helpers/axios-helper.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     //We are in the server
     return axios.create({
-      baseURL: "http://nginx-ingress-controller.kube-system.svc.cluster.local",
+      //baseURL: "http://nginx-ingress-controller.kube-system.svc.cluster.local",
+      baseURL: "http://www.ticketing.host",
       headers: req.headers,
     });
   } else {
