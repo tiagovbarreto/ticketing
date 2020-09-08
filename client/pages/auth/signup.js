cd016b71fddfea098e18403251b0329e3ac174a3
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-09-08 18:40:48 -0300

    ...

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
-export default () => {
+const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { doRequest, errors } = useRequest({
@@ -45,3 +45,5 @@ export default () => {
     </form>
   );
 };
+
+export default Signup;
